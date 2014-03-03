7d9eaa2c4ab618ad5cf1b6fb544cf173ab88442b
om37 <om37@uni.brighton.ac.uk>
2014-03-03 16:58:03 +0000

    Added multicastplayer class

---
+++ pongSolutionV3/src/client/C_PongMulticastPlayer.java
@@ -0,0 +1,148 @@
+package client;
+
+import common.*;
+import static common.Global.*;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Date;
+/**
+ * Individual player run as a separate thread to allow
+ * updates immediately the bat is moved
+ */
+class C_PongMulticastPlayer extends C_PongPlayer
+{
+	private NetObjectReader reader;
+	private NetObjectWriter writer;
+	private NetMCReader		mcReader;
+	private String 			moveDir;
+	private C_PongModel model;
+	
+  /**
+   * Constructor
+   * @param model - model of the game
+   * @param s - Socket used to communicate with server
+   */
+	
+  public C_PongMulticastPlayer(C_PongModel aModel, Socket s)
+  {
+	  model=aModel;
+
+	  try 
+	  {
+		reader = new NetObjectReader(s);
+		writer = new NetObjectWriter(s);
+		mcReader = new NetMCReader(Global.P_SERVER_WRITE,Global.MCA);
+	  }
+	  catch (Exception e) 
+	  {
+		  DEBUG.trace("PlayerC constructor failed" );
+	  }
+  }
+  
+  /**
+   * Get and update the model with the latest bat movement
+   * sent by the server
+   */
+  public void run()                             // Execution
+  {
+	  DEBUG.trace("Started player thread....");
+	  int gameNum;
+	  while(true)
+	  {
+		  String stringNum = (String)reader.get();
+		  try
+		  {
+			  gameNum = Integer.parseInt(stringNum);
+			  break;
+		  }
+		  catch(NumberFormatException e)
+		  {}
+	  }
+	  
+    // Listen to network to get the latest state of the
+    //  game from the server
+    // Update model with this information, Redisplay model
+	
+		  System.out.println(gameNum);
+		  while(true)
+		  {		
+			String data="";
+			try
+			{
+				data = (String) mcReader.get();
+				DEBUG.trace( "Client PLayer read %s", data);
+			}
+			catch (IOException e)
+			{
+				DEBUG.error("MC In.gett error: %s", e.getMessage());
+				e.printStackTrace();
+			}
+			
+			String[] newCoords = data.split(",");
+			
+				if(newCoords[0].equals("Game "+gameNum))//Is it the game we're watching?
+				{
+					updateViewWithNewValues(data);
+					//Thread.sleep(20);
+				}
+			
+		  	  
+	  }
+  }
+
+	private void updateViewWithNewValues(String inData)
+	{
+		String data=inData;
+		
+		String[] newCoords = data.split(",");
+		
+		//Decode string...
+        //String gameNumber = newCoords[0];					//0: gameNo 
+		double newBallX=Double.parseDouble(newCoords[1]);	//1: ballX
+		double newBallY=Double.parseDouble(newCoords[2]);	//2: ballY
+		
+		double batOneX=Double.parseDouble(newCoords[3]);	//3: bat0X
+		double batOneY=Double.parseDouble(newCoords[4]);	//4: bat0Y
+		
+		double batTwoX=Double.parseDouble(newCoords[5]);	//5: bat1X
+		double batTwoY=Double.parseDouble(newCoords[6]);	//6: bat1Y
+		
+		
+		
+		//Create dummy ball object with new coords 
+		//GameObject newBall = model.getBall();
+		GameObject newBall   = new GameObject( W/2, H/2, BALL_SIZE, BALL_SIZE );
+		newBall.setX(newBallX);
+		newBall.setY(newBallY);
+		
+		//Same for bats....
+		//GameObject[] newBats = model.getBats();
+		GameObject newBats[] = new GameObject[2];
+		newBats[0] = new GameObject(  60, H/2, BAT_WIDTH, BAT_HEIGHT);
+		newBats[1] = new GameObject(W-60, H/2, BAT_WIDTH, BAT_HEIGHT);
+		
+		newBats[0].setX(batOneX);
+		newBats[0].setY(batOneY);
+		
+		newBats[1].setX(batTwoX);
+		newBats[1].setY(batTwoY);
+		
+		//Send to model:
+		model.setBall(newBall);
+		model.setBats(newBats);
+		
+		//Call to update observers
+		model.modelChanged();
+	}
+  
+  public void moveBat(String details)
+  {
+	  System.out.println(details);
+	  long date = new Date().getTime();
+	  writer.put(details+","+date);
+	  model.setChanged(true);
+	  //DEBUG.trace("set changed");
+  }
+  	
+}
